3ed3a888441abed3ccd5bdb079a00141fb474abf
Foysal79 <onthor312001@gmail.com>
2023-09-13 21:51:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-5xl font-bold text-center mt-10'>Knowledge Cafe</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      
+    </>
+  )
+}
+
+export default App
